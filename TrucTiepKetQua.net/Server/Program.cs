cfd414cd722a010f01b06251bca64e6e360923b0
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2022-04-11 09:39:16 +0700

    Add controller lottery

--- TrucTiepKetQua.net/Server/Program.cs
+++ TrucTiepKetQua.net/Server/Program.cs
@@ -7,9 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+#if DEBUG
+Console.WriteLine("DEBUG");
+#else
 builder.Services.AddHostedService<KetQuaXoSoMienBacService>();
 builder.Services.AddHostedService<KetQuaXoSoMienNamService>();
 builder.Services.AddHostedService<KetQuaXoSoMienTrungService>();
+
+#endif
 
 var app = builder.Build();
 
