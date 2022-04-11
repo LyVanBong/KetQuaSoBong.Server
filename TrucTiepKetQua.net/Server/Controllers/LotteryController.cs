2d710589a11f6bb22c01dcf3f4e4c4d6c6d93ce2
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2022-04-11 11:00:49 +0700

    Thêm api lấy dữ liệu kết quả xổ số 3 miền

--- TrucTiepKetQua.net/Server/Controllers/LotteryController.cs
+++ TrucTiepKetQua.net/Server/Controllers/LotteryController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using TrucTiepKetQua.net.Server.Configurations;
+using TrucTiepKetQua.net.Shared.Models;
 
 namespace TrucTiepKetQua.net.Server.Controllers
 {
@@ -8,36 +10,73 @@ namespace TrucTiepKetQua.net.Server.Controllers
     [ApiController]
     public class LotteryController : ControllerBase
     {
-        // GET: api/<LotteryController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private IMongoCollection<KqxsMbModel> _collectionMb;
+        private IMongoCollection<KqxsMnModel> _collectionMn;
+        private IMongoCollection<KqxsMnModel> _collectionMt;
+
+        private string[] _region = new string[]
         {
-            return new string[] { "value1", "value2" };
+            "northern","central","south"
+        };
+        public LotteryController()
+        {
+            MongoClient mongo = new MongoClient(AppConstants.ConnectionStringMongoDb);
+            IMongoDatabase database = mongo.GetDatabase("Kqxs");
+            _collectionMb = database.GetCollection<KqxsMbModel>("KqxsMb");
+            _collectionMn = database.GetCollection<KqxsMnModel>("KqxsMn");
+            _collectionMt = database.GetCollection<KqxsMnModel>("KqxsMt");
         }
 
-        // GET api/<LotteryController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        /// <summary>
+        /// Lấy kết quả xổ số 3 miền theo ngày
+        /// </summary>
+        /// <param name="region">Miền cần lấy
+        /// "northern","central","south"
+        /// </param>
+        /// <param name="date">Ngày cần lấy kết quả
+        /// d-m-yyyy
+        /// </param>
+        /// <returns>
+        /// Kết Quả ngày hôm đó
+        /// </returns>
+        [HttpGet("{region}/{date}")]
+        public IActionResult Get(string region, string date)
         {
-            return "value";
-        }
+            if (!string.IsNullOrEmpty(region) && !string.IsNullOrEmpty(date))
+            {
+                if (_region.Contains(region))
+                {
+                    if (DateTime.TryParse(date, new CultureInfo("vi-VN"), DateTimeStyles.None, out DateTime result))
+                    {
+                        if (region == _region[0])
+                        {
+                            var data = _collectionMb.Find(x => x.NgayQuay == date).First();
+                            if (data != null)
+                            {
+                                return Ok(data);
+                            }
+                        }
+                        else if (region == _region[1])
+                        {
+                            var data = _collectionMt.Find(x => x.NgayQuay == date).First();
+                            if (data != null)
+                            {
+                                return Ok(data);
+                            }
+                        }
+                        else if (region == _region[2])
+                        {
+                            var data = _collectionMn.Find(x => x.NgayQuay == date).First();
+                            if (data != null)
+                            {
+                                return Ok(data);
+                            }
+                        }
+                    }
+                }
+            }
 
-        // POST api/<LotteryController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<LotteryController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<LotteryController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return BadRequest();
         }
     }
 }
